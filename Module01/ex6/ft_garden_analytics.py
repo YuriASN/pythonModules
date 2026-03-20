e5aa36da0e4f69e2b6f35fd08ab9bfb80def054d
Yuri ASN <yuriasn@hotmail.com>
2026-03-20 22:45:59 +0000

    plant classes done

--- Module01/ex6/ft_garden_analytics.py
+++ Module01/ex6/ft_garden_analytics.py
@@ -1,26 +1,22 @@
-from typing import List
-
-
 class Plant:
-    def __init__(self, name: str, height: int, days_old: int):
-        self.__name = name
-        self.__height = 0
-        self.__growth = 0
-        self.__days_old = 0
+    def __init__(self, name: str, height: int, days_old: int) -> None:
+        self.__name: str = name
+        self.__height: int = 0
+        self.__growth: int = 0
+        self.__days_old: int = 0
         self.set_height(height)
         self.set_age(days_old)
 
-    def get_type(self):
-        return f"{self.__class__.__name__}"
+    def grow(self, size: int) -> str:
+        if size > 0:
+            self.__growth += size
+            return f"{self.__name} grew {size}cm"
 
-    def grow(self) -> None:
-        self.__growth += 1
-
-    def get_growth(self):
-        return self.__growth
-
-    def age(self) -> None:
-        self.__days_old += 1
+    def age(self, days: int) -> str:
+        if days > 0:
+            self.__days_old += days
+            return (f"{self.__name} aged {days} days, " +
+                    f"and it's now {self.__days_old} days old")
 
     def get_name(self) -> str:
         return f"{self.__name}"
@@ -30,6 +26,12 @@ class Plant:
 
     def get_height(self) -> int:
         return self.__height + self.__growth
+
+    def get_growth(self) -> int:
+        return self.__growth
+
+    def get_type(self) -> str:
+        return f"{self.__class__.__name__}"
 
     def set_age(self, new_age: int) -> None:
         if new_age > 0:
@@ -55,17 +57,14 @@ class FloweringPlant(Plant):
         super().__init__(name, height, days_old)
         self.__color = color
 
-    def get_type(self):
-        return f"{self.__class__.__name__}"
-
     def get_color(self) -> str:
         return f"{self.__color}"
 
     def bloom(self) -> str:
-        if super().get_height() < 10:
-            return (f"not blooming")
+        if self.get_age() < 10:
+            return "not blooming"
         else:
-            return (f"blooming")
+            return "blooming"
 
     def get_info(self) -> str:
         return (super().get_info() +
@@ -73,64 +72,28 @@ class FloweringPlant(Plant):
 
 
 class PrizeFlower(FloweringPlant):
-    def __init__(self, name: str, height: int, days_old: int, color: str,
-                 prize_pnts: int):
+    def __init__(self, name: str, height: int,
+                 days_old: int, color: str, prize: int):
         super().__init__(name, height, days_old, color)
-        self.__prize_p = prize_pnts
-
-    def get_type(self):
-        return f"{self.__class__.__name__}"
+        self.__prize = prize
 
     def get_prize_points(self) -> int:
-        return f"{self.__prize_p}"
-
-    def set_prize_points(self, new_points) -> str:
-        self.__prize_p = new_points
+        return self.__prize
 
     def get_info(self) -> str:
         return (super().get_info() +
                 f", Prize points: {self.get_prize_points()}")
 
 
-class Garden():
-    def __init__(self, owner: str, plants: List[Plant]):
-        self.__owner = owner
-        self.__plants = plants
+#class GardenManager():
 
-    def get_owner(self):
-        return f"{self.__owner}"
+#    class GardenStats():
 
-    def add_plant(self, new: Plant):
-        self.__plants.append(new)
-        print(f"Added {new.get_name()} to {self.get_owner()}'s garden")
 
-    def total_gain(self):
-        total_gain = 0
-        for plant in self.__plants:
-            total_gain += plant.get_growth()
-        return total_gain
-
-    def garden_report(self):
-        plant_amount: int = 0
-        class_count: List
-        print(f"=== {self.get_owner()}'s Garden Report ===\n"
-              f"Plants in garden:")
-        for plant in self.__plants:
-            print(f"- {plant.get_info()}")
-            plant_amount += 1
-            class_count[plant.get_type()] += 1
-        print(f"\nPlants added: {self.__plants.count()}, "
-              f"Total growth {self.total_gain()}cm")
-        print(f"Plant types: ")
-        for type in class_count:
-            
-        {} regular, "
-              f"{} flowering, "
-              f"{} prize flowers")
-
-class GardenManager():
-    def __init__(self, gardens: Garden):
-        
-    def create_garden_network():
-
-    def GardenStats():
+if __name__ == "__main__":
+    pflower = PrizeFlower("Rose", 10, 10, "Blue", 10)
+    nflower = FloweringPlant("Tulip", 5, 5, "white")
+    plant = Plant("Avocado Tree", 50, 100)
+    print(f"Type of pflower = {pflower.get_type()}\n"
+          f"Type of nflower = {nflower.get_type()}\n"
+          f"Type of plant = {plant.get_type()}")
