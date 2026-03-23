c1dd9455b0406c16a14a32807b25930e508adc54
Yuri Augusto Santos Nunes <ysantos-@student.42lisboa.com>
2026-03-23 12:55:24 +0000

    Done with comments and flake8 ok

--- Module01/ex6/ft_garden_analytics.py
+++ Module01/ex6/ft_garden_analytics.py
@@ -4,36 +4,39 @@ class Plant:
         self.__height: int = 0
         self.__growth: int = 0
         self.__days_old: int = 0
-        self.set_height(height)
-        self.set_age(days_old)
+        self.__set_height(height)
+        self.__set_age(days_old)
 
     def grow(self, size: int) -> str:
+        """Grow the plant by `size` centimeters"""
+
         if size > 0:
             self.__growth += size
             return f"{self.__name} grew {size}cm"
 
-    def age(self, days: int) -> str:
-        if days > 0:
-            self.__days_old += days
-            return (f"{self.__name} aged {days} days, " +
-                    f"and it's now {self.__days_old} days old")
-
     def get_name(self) -> str:
+        """"Returns the name of the Plant"""
         return f"{self.__name}"
 
     def get_age(self) -> int:
+        """Returns how many days old the plant is"""
         return self.__days_old
 
     def get_height(self) -> int:
+        """Returns the full height of the plant (original height + growth)"""
         return self.__height + self.__growth
 
     def get_growth(self) -> int:
+        """Returns the growth that the plant already had"""
         return self.__growth
 
     def get_type(self) -> str:
+        """Returns the type of the plant"""
         return f"{self.__class__.__name__}"
 
-    def set_age(self, new_age: int) -> None:
+    def __set_age(self, new_age: int) -> None:
+        """Set the first age of the plant, if value is bigger than 0"""
+
         if new_age > 0:
             self.__days_old = new_age
         else:
@@ -41,7 +44,9 @@ class Plant:
                   f"\nSecurity: Negative age rejected\n\nCurrent plant: "
                   f"{self.__name} ({self.__height}cm, {self.__days_old} days)")
 
-    def set_height(self, new_height: int) -> None:
+    def __set_height(self, new_height: int) -> None:
+        """Set the first height of the plant if value is bigger than 0"""
+
         if new_height > 0:
             self.__height = new_height
         else:
@@ -49,6 +54,7 @@ class Plant:
                   f"[REJECTED]\nSecurity: Negative height rejected")
 
     def get_info(self) -> str:
+        """Returns the info of all main data of the plant"""
         return (f"{self.__name}: {self.__height}cm")
 
 
@@ -58,15 +64,20 @@ class FloweringPlant(Plant):
         self.__color = color
 
     def get_color(self) -> str:
+        "Returns the color of the plant"
         return f"{self.__color}"
 
     def bloom(self) -> str:
+        """Returns if flower is blooming or not, accordingly to it's age"""
+
         if self.get_age() < 10:
             return "not blooming"
         else:
             return "blooming"
 
     def get_info(self) -> str:
+        """Returns the info of all main data of the plant"""
+
         return (super().get_info() +
                 f", {self.get_color()} flowers ({self.bloom()})")
 
@@ -78,9 +89,11 @@ class PrizeFlower(FloweringPlant):
         self.__prize = prize
 
     def get_prize_points(self) -> int:
+        """Returns the prize points of the plant"""
         return self.__prize
 
     def get_info(self) -> str:
+        """Returns the info of all main data of the plant"""
         return (super().get_info() +
                 f", Prize points: {self.get_prize_points()}")
 
@@ -94,31 +107,42 @@ class Garden:
             self.__plant_amount += 1
 
     def get_owner(self) -> str:
+        """Returns the name of the owner of the garden"""
         return f"{self.__owner}"
 
     def get_plants(self) -> list[Plant]:
+        """Returns the list of plants on the garden"""
         return self.__plants
 
     def get_amount_plants(self) -> int:
+        """Returns the amount of plants that the garden has"""
         return self.__plant_amount
 
     def add_plant(self, plant: Plant) -> None:
+        """Adds a new plant to the garden"""
+
         self.__plants += [plant]
         self.__plant_amount += 1
 
     def all_height(self) -> int:
+        """Return the sum of the full height of all plants"""
+
         total: int = 0
         for plant in self.__plants:
             total += plant.get_height()
         return total
 
     def all_growth(self) -> int:
+        """Return the sum of the growth of all plants"""
+
         total: int = 0
         for plant in self.__plants:
             total += plant.get_growth()
         return total
 
     def all_prize(self) -> int:
+        """Return the sum of all prize points for every plant that has it"""
+
         total: int = 0
         for plant in self.__plants:
             if plant.get_type() == "PrizeFlower":
@@ -126,7 +150,13 @@ class Garden:
         return total
 
     def reset_plants(self, new_list: list[Plant]) -> None:
+        """Reset the list of plants in the garden for the new_list passed"""
+
         self.__plants = new_list
+        total: int = 0
+        for plant in new_list:
+            total += 1
+        self.__plant_amount = total
 
 
 class GardenManager:
@@ -160,17 +190,29 @@ class GardenManager:
 
     def report(self, owner: str) -> None:
         """Calls function of helper class passing the Garden as argument"""
+
         for garden in self.__gardens:
             if garden.get_owner() == owner:
                 self.GardenStats.report(garden)
 
     def add_garden(self, garden: Garden) -> None:
         """Add garden to be managed"""
+
         self.__gardens += [garden]
         self.__total_gardens += 1
 
+    def get_garden(self, owner: str) -> Garden:
+        """Return the 1st occurence of the garden of owner,
+        or None if not found"""
+
+        for garden in self.__gardens:
+            if garden.get_owner() == owner:
+                return garden
+        return None
+
     def grow_all(self, owner: str, growth: int) -> None:
         """Grow all the plants from owner's garden by `growth` centimeters"""
+
         if growth < 1:
             pass
         for garden in self.__gardens:
@@ -186,6 +228,7 @@ class GardenManager:
 
     def add_plant(self, owner: str, plant: Plant) -> None:
         """Adds the plant to the owner's garden"""
+
         for garden in self.__gardens:
             if garden.get_owner() == owner:
                 garden.add_plant(plant)
@@ -195,6 +238,7 @@ class GardenManager:
 
     def del_plant(self, owner: str, remove: str) -> None:
         """Re-do the plant list on owner's garden removing any 'delete'"""
+
         for garden in self.__gardens:
             if garden.get_owner() == owner:
                 plants = garden.get_plants()
@@ -205,8 +249,9 @@ class GardenManager:
                 garden.reset_plants(new_list)
                 break
 
-    def del_garden(self, owner: str):
+    def del_garden(self, owner: str) -> None:
         """Remove the garden of the owner"""
+
         self.__gardens = [
             garden for garden in self.__gardens
             if garden.get_owner() != owner
@@ -214,6 +259,7 @@ class GardenManager:
 
     def scores(self) -> None:
         """Print the scores for each garden managed"""
+
         amount = self.__total_gardens
         print("Garden scores - ", end="")
         i = 0
@@ -230,6 +276,7 @@ class GardenManager:
     @staticmethod
     def height_validation(height: int) -> str:
         """Validates if height value is positive"""
+
         if height < 0:
             return "False"
         else:
@@ -238,12 +285,9 @@ class GardenManager:
     @classmethod
     def create_garden_network(cls,
                               gardens: list[Garden]
-                              ) -> list["GardenManager"]:
-        """Creates a manager per garden????"""
-        managers: list["GardenManager"] = []
-        for garden in gardens:
-            managers += [cls(garden)]
-        return managers
+                              ) -> "GardenManager":
+        """Creates a manager for the list of gardens"""
+        return cls(gardens)
 
 
 if __name__ == "__main__":
@@ -274,3 +318,7 @@ if __name__ == "__main__":
     print(f"Height validation test: {manager.height_validation(10)}")
     manager.scores()
     print(f"Total gardens managed: {manager.gardens_managed()}")
+    print("New manager with Cat and Mari's garden:")
+    new_man = manager.create_garden_network([manager.get_garden("Mari"),
+                                             manager.get_garden("Cat")])
+    new_man.scores()
