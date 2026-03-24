ad1e75868bb7d8a40062adc48d4cfb325359fc35
Yuri ASN <yuriasn@hotmail.com>
2026-03-24 10:35:26 +0000

    Fixed for new subject

--- Module01/ex1/ft_garden_data.py
+++ Module01/ex1/ft_garden_data.py
@@ -1,5 +1,4 @@
-from typing import List
-
+#!/usr/bin/env python3
 
 class Plant:
     def __init__(self, name: str, height: int, age: int):
@@ -7,18 +6,21 @@ class Plant:
         self.height = height
         self.age = age
 
+    def show(self) -> None:
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
 
-def ft_garden_data(garden: List[Plant]) -> None:
+
+def ft_garden_data(garden: list[Plant]) -> None:
     print("=== Garden Plant Registry ===")
     for i in garden:
-        print(f"{i.name}: {i.height}cm, {i.age} days old")
+        i.show()
 
 
 def main() -> None:
     plant1 = Plant("Roses", 15, 9)
     plant2 = Plant("Sunflower", 34, 6)
     plant3 = Plant("Tulips", 2, 3)
-    garden: List[Plant] = [plant1, plant2, plant3]
+    garden: list[Plant] = [plant1, plant2, plant3]
     ft_garden_data(garden)
 
 
--- Module01/ex2/ft_plant_growth.py
+++ Module01/ex2/ft_plant_growth.py
@@ -1,31 +1,31 @@
+#!/usr/bin/env python3
+
 class Plant:
-    def __init__(self, name: str, height: int, days_old: int):
+    def __init__(self, name: str, height: float, days_old: int):
         self.name = name
         self.height = height
         self.days_old = days_old
 
-    def grow(self) -> None:
-        self.height += 1
+    def grow(self, growth: float) -> None:
+        self.height += growth
 
-    def age(self) -> None:
-        self.days_old += 1
+    def age(self, days_passed: int) -> None:
+        self.days_old += days_passed
 
-    def get_info(self) -> str:
-        return f"{self.name}: {self.height}cm, {self.days_old} days old"
+    def show(self) -> None:
+        print(f"{self.name}: {self.height:.1f}cm, {self.days_old} days old")
 
 
 def week_growth(plant: Plant) -> None:
     first_height = plant.height
-    print("=== Day 1 ===")
-    print(plant.get_info())
-    for i in range(1, 7):
-        plant.grow()
-        plant.age()
-    print("=== Day 7 ===")
-    print(plant.get_info())
-    print(f"Growth this week: +{plant.height - first_height}cm")
+    for i in range(1, 8):
+        print(f"=== Day {i} ===")
+        plant.show()
+        plant.grow(0.8)
+        plant.age(1)
+    print(f"Growth this week: +{plant.height - first_height:.0f}cm")
 
 
 if __name__ == "__main__":
-    rose = Plant("Rose", 10, 20)
+    rose = Plant("Rose", 25, 30)
     week_growth(rose)
