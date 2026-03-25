7fbcac3524399f1939f49ab6ef4717a13a3878bc
Yuri Augusto Santos Nunes <ysantos-@student.42lisboa.com>
2026-03-25 20:31:01 +0000

    Finished ex4

--- Module02/ex4/ft_finally_block.py
+++ Module02/ex4/ft_finally_block.py
@@ -6,8 +6,7 @@ class GardenError(Exception):
 class PlantError(GardenError):
     """Base class for plant errors"""
     def __init__(self, plant: str, error: str) -> None:
-        if error is None:
-            error = f"The {plant} plant is wilting!"
+        error = f"Invalid plant name to water: '{plant}'"
         super().__init__(error)
 
 
@@ -21,34 +20,72 @@ class WaterError(GardenError):
 
 class Plant:
     def __init__(self, name: str, height: int, days_old: int) -> None:
-        self.__name = name
-        self.__height = height
-        self.__days_old = days_old
-        self.__water = 4
+        self._name = name
+        self._height = height
+        self._days_old = days_old
+        self._water = 4
 
     def get_name(self) -> str:
-        return f"{self.__name}"
+        """Returns the name of the plant"""
+        return f"{self._name}"
 
     def get_status(self) -> str:
-        if self.__water >= 5:
-            return f"{self.__name} is looking good"
+        """Returns a string of the status of the plan,
+        accordingly to the amount of water that it has"""
+        if self._water < 5:
+            return f"{self._name} is looking good"
         else:
-            raise PlantError(self.__name, None)
+            raise PlantError(self._name, None)
 
     def pass_time(self) -> None:
         """Pass 5 days, growing 1cm and losing 1 water"""
-        self.__days_old += 5
-        self.__height += 1
-        self.__water -= 1
+        self._days_old += 5
+        self._height += 1
+        self._water -= 1
 
     def watering(self) -> None:
         """Adds water by 1"""
-        self.__water += 1
+        self._water += 1
 
 
-def water_plant(plant_name: Plant) -> None:
+def water_plant(plant: Plant) -> None:
     """Waters the plant if plant name is captalized"""
+    name = plant.get_name()
+    if name == name.capitalize():
+        plant.watering()
+        print(f"Watering {plant.get_name()}: [OK]")
+    else:
+        raise PlantError(plant.get_name(), None)
 
 
-def test_watering_system() -> None:
+def test_watering_system(plants: list[Plant]) -> None:
     """Water several Plants using water_plant()"""
+
+    try:
+        print("Opening watering system")
+        for plant in plants:
+            water_plant(plant)
+    except PlantError as err:
+        print(f"Caught PlantError: {err}")
+        print(".. ending tests and returning to main")
+    finally:
+        print("Closing watering system")
+
+
+def main() -> None:
+
+    print("=== Garden Watering System ===\n")
+    plants: list = [Plant("Tomato", 10, 10),
+                    Plant("Lettuce", 10, 10),
+                    Plant("Carrots", 10, 10)]
+    print("Testing valid plants...")
+    test_watering_system(plants)
+    plants[1]._name = "lettuce"
+    print("\nTesting invalid plants...")
+    test_watering_system(plants)
+
+    print("\nCleanup always happens, even with errors!")
+
+
+if __name__ == "__main__":
+    main()
