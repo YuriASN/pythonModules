feb038855391367883f4b169f58bba575587550d
Yuri Augusto Santos Nunes <ysantos-@student.42lisboa.com>
2026-06-16 17:26:03 +0100

    Fixed try except

--- Module07/battle.py
+++ Module07/battle.py
@@ -4,34 +4,43 @@ from ex0 import CreatureFactory, FlameFactory, AquaFactory
 
 
 def test_factory(factory: CreatureFactory) -> None:
-    factory.create_base()
-    print(factory.base.describe())
-    print(factory.base.attack())
-    factory.create_evolved()
-    print(factory.evolved.describe())
-    print(factory.evolved.attack())
+    try:
+        factory.create_base()
+        print(factory.base.describe())
+        print(factory.base.attack())
+        factory.create_evolved()
+        print(factory.evolved.describe())
+        print(factory.evolved.attack())
+    except Exception as err:
+        Exception(f"Testing factory: {err}")
 
 
 def test_battle(factory_a: CreatureFactory,
                 factory_b: CreatureFactory) -> None:
-    print(factory_a.base.describe())
-    print(" vs.")
-    print(factory_b.base.describe())
-    print(" fight!")
-    print(factory_a.base.attack())
-    print(factory_b.base.attack())
+    try:
+        print(factory_a.base.describe())
+        print(" vs.")
+        print(factory_b.base.describe())
+        print(" fight!")
+        print(factory_a.base.attack())
+        print(factory_b.base.attack())
+    except Exception as err:
+        raise Exception(f"Testing battle: {err}")
 
 
 if __name__ == "__main__":
-    print("Testing factory")
-    flame_f = FlameFactory()
-    test_factory(flame_f)
-    print()
+    try:
+        print("Testing factory")
+        flame_f = FlameFactory()
+        test_factory(flame_f)
+        print()
 
-    print("Testing factory")
-    aqua_f = AquaFactory()
-    test_factory(aqua_f)
-    print()
+        print("Testing factory")
+        aqua_f = AquaFactory()
+        test_factory(aqua_f)
+        print()
 
-    print("Testing battle")
-    test_battle(flame_f, aqua_f)
+        print("Testing battle")
+        test_battle(flame_f, aqua_f)
+    except Exception as err:
+        print(err)
--- Module07/capacitor.py
+++ Module07/capacitor.py
@@ -3,27 +3,30 @@
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
 
 if __name__ == "__main__":
-    # Healing
-    print("Testing Creature with healing capability")
-    healing_f = HealingCreatureFactory()
-    healing_f.create_base()
-    print(f" base:\n{healing_f.base.describe()}\n"
-          f"{healing_f.base.attack()}\n{healing_f.base.heal()}")
-    healing_f.create_evolved()
-    print(f" evolved:\n{healing_f.evolved.describe()}\n"
-          f"{healing_f.evolved.attack()}\n{healing_f.evolved.heal()}")
-    print()
+    try:
+        # Healing
+        print("Testing Creature with healing capability")
+        healing_f = HealingCreatureFactory()
+        healing_f.create_base()
+        print(f" base:\n{healing_f.base.describe()}\n"
+              f"{healing_f.base.attack()}\n{healing_f.base.heal()}")
+        healing_f.create_evolved()
+        print(f" evolved:\n{healing_f.evolved.describe()}\n"
+              f"{healing_f.evolved.attack()}\n{healing_f.evolved.heal()}")
+        print()
 
-    # Transformed
-    print("Testing Creature with transform capability")
-    trans_f = TransformCreatureFactory()
-    trans_f.create_base()
-    print(f" base:\n{trans_f.base.describe()}\n"
-          f"{trans_f.base.attack()}\n"
-          f"{trans_f.base.transform()}\n{trans_f.base.attack()}\n"
-          f"{trans_f.base.revert()}")
-    trans_f.create_evolved()
-    print(f" evolved:\n{trans_f.evolved.describe()}\n"
-          f"{trans_f.evolved.attack()}\n"
-          f"{trans_f.evolved.transform()}\n{trans_f.evolved.attack()}\n"
-          f"{trans_f.evolved.revert()}")
+        # Transformed
+        print("Testing Creature with transform capability")
+        trans_f = TransformCreatureFactory()
+        trans_f.create_base()
+        print(f" base:\n{trans_f.base.describe()}\n"
+              f"{trans_f.base.attack()}\n"
+              f"{trans_f.base.transform()}\n{trans_f.base.attack()}\n"
+              f"{trans_f.base.revert()}")
+        trans_f.create_evolved()
+        print(f" evolved:\n{trans_f.evolved.describe()}\n"
+              f"{trans_f.evolved.attack()}\n"
+              f"{trans_f.evolved.transform()}\n{trans_f.evolved.attack()}\n"
+              f"{trans_f.evolved.revert()}")
+    except Exception as err:
+        print(err)
