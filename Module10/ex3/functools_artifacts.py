85ba43c9b9e9b8d2f83486004e571f24f0ab7b2a
Yuri Augusto Santos Nunes <ysantos-@student.42lisboa.com>
2026-06-22 22:00:26 +0100

    ongoing

--- Module10/ex3/functools_artifacts.py
+++ Module10/ex3/functools_artifacts.py
@@ -26,11 +26,9 @@ def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
         if not callable(base_enchantment):
             raise TypeError("Base Enchantment isn't callable")
         functions = {
-            "water": functools.partial(base_enchantment(power=50,
-                                                        element="water")),
-            "fire": functools.partial(base_enchantment(power=50,
-                                                       element="fire")),
-            "air": functools.partial(base_enchantment(power=50, element="air"))
+            "water": functools.partial(base_enchantment, 50, "water"),
+            "fire": functools.partial(base_enchantment, 50, "fire"),
+            "air": functools.partial(base_enchantment, 50, "air")
         }
     except Exception as err:
         raise Exception(f"partial_enchanter: {err}")
@@ -46,31 +44,53 @@ def memoized_fibonacci(n: int) -> int:
     return memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2)
 
 
-@functools.singledispatch
-def spell_dispatcher(spell: Any) -> Callable[[Any], str]:
-    return lambda spell: "Unknown spell type"
+def spell_dispatcher() -> Callable[[Any], str]:
+    @functools.singledispatch
+    def dispatch(spell: Any) -> str:
+        return "Unknown spell type"
 
+    @dispatch.register
+    def _(spell: int) -> str:
+        return f"Damage spell: {spell} damage"
 
-@spell_dispatcher
-def _(spell: int) -> str:
-    return f"Damage spell: {spell} damage"
+    @dispatch.register
+    def _(spell: str) -> str:
+        return f"Enchantment: {spell}"
 
+    @dispatch.register
+    def _(spell: list) -> str:
+        return f"Multicast: {len(spell)} spells"
 
-@spell_dispatcher
-def _(spell: str) -> str:
-    return f"Enchantment: {spell}"
-
-
-@spell_dispatcher
-def _(spell: list) -> str:
-    return f"Multicast: {len(spell)} spells"
+    return dispatch
 
 
 if __name__ == "__main__":
     spell_powers = [25, 41, 36, 11, 24, 10]
-    operations = ['add', 'multiply', 'max', 'min']
-    fibonacci_tests = [8, 20, 13]
+    operations = ["add", "multiply", "max", "min"]
     print("=== Testing spell reducer... ===")
-    print("=== Testing partial enchanter... ===")
-    print("=== Testing memoized fibonacci... ===")
-    print("=== Testing spell dispatcher... ===")
+    for op in operations:
+        print(f"{op}: {spell_reducer(spell_powers, op)}")
+
+    print("\n=== Testing partial enchanter... ===")
+
+    def enchanter(power: int, element: str, target: str) -> str:
+        return f"Pow: {power}, Elem: {element}, target: {target}"
+
+    partialized = partial_enchanter(enchanter)
+    print(f"{partialized['water']('Charmander')}\n"
+          f"{partialized['fire']('Bulbasaur')}\n"
+          f"{partialized['air']('Pikachu')}\n")
+
+    print("\n=== Testing memoized fibonacci... ===")
+    print(f"\t{memoized_fibonacci.cache_info()}")
+    print(f"{memoized_fibonacci(13)}\n\t{memoized_fibonacci.cache_info()}")
+    print(f"{memoized_fibonacci(13)}\n\t{memoized_fibonacci.cache_info()}")
+    print(f"{memoized_fibonacci(23)}\n\t{memoized_fibonacci.cache_info()}")
+    print(f"{memoized_fibonacci(13)}\n\t{memoized_fibonacci.cache_info()}")
+
+    print("\n=== Testing spell dispatcher... ===")
+    dispatch = spell_dispatcher()
+    print(f"{dispatch(42)}\n"
+          f"{dispatch('fireball')}\n"
+          f"{dispatch(['fireball', 'AquaJet', 'Freeze'])}\n"
+          f"{dispatch(None)}")
